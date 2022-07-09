84887549115fa0cf85c7ac33848da0e34022eb45
Isma Rianto <ismarianto97@gmail.com>
2022-07-09 11:24:49 +0700

    update data login

--- components/form.js
+++ components/form.js
@@ -8,39 +8,53 @@ const Form = ({ value, handleChange, handleSubmit }) => {
 
         <>
             <div className="container" style={{
-                'margin-top': '60px'
+                'margin-top': '60px',
+
+
             }}>
                 <div className="row">
-                    <div className="col-md-12">
+                    <div className="col-md-6">
                         <div className="card">
-                            <h3 className="heading">Login Aplikasi</h3>
-                            <div className="card-body">
+                            <div className="card-header" style={{
+
+                                'background': '#1b5bab',
+                                'color': '#fff',
+                                'text-align': 'center'
+                            }}>
+                                <h3 className="heading"><b><i className="fa fa-users"></i> Login Aplikasi</b></h3>
+                            </div>
+                            <div className="card-body" style={{ 'magin-right': '12px' }}>
 
                                 <form action="" className="form-horizontal" method="POST" onSubmit={handleSubmit}>
                                     <div className="form-group row">
-                                        <label className="col-md-2 label">Username</label>
-                                        <div className="col-md-3">
+                                        <label className="col-md-3 label"><b>Username</b></label>
+                                        <div className="col-md-5">
                                             <input type={'text'} name="username" className="form-control" onChange={handleChange} value={value.username} required></input>
                                         </div>
                                     </div>
                                     <br />
                                     <div className="form-group row">
-                                        <label className="col-md-2">Password</label>
-                                        <div className="col-md-3">
+                                        <label className="col-md-3 label"><b>Password</b></label>
+                                        <div className="col-md-5">
                                             <input type={'password'} name="password" className="form-control" onChange={handleChange} value={value.password} required></input>
                                         </div>
                                     </div>
+                                    <br />
                                     <div className="form-group row">
-                                        <div className="col-md-3">
-                                            <button className="btn btn-primary btn-sm" type="submit"><i className="fa fa-save"></i> Login</button>
+
+                                        <div className="col-md-8">
+                                            <button className="btn btn-primary btn-md" type="submit"><i className="fa fa-save"></i> Login</button>
                                             &nbsp; &nbsp;
-                                            <button className="btn btn-warning btn-sm" type="reset"><i className="fa fa-save"></i> Reset</button>
+                                            <button className="btn btn-warning btn-md" type="reset"><i className="fa fa-save"></i> Reset</button>
                                         </div>
                                     </div>
 
                                 </form>
+                                <br />
                             </div>
                         </div>
+                    </div>
+                    <div className="col-md-6">
                     </div>
                 </div>
             </div>
